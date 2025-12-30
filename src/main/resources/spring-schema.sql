6139fa85bef89650827c64a3031fe4fd43a8a729
rdurgesh <drai548@gmail.com>
2025-12-30 20:07:13 +0530

    Integration test completed

--- src/main/resources/spring-schema.sql
+++ src/main/resources/spring-schema.sql
@@ -73,14 +73,3 @@ CREATE TABLE bills (
 CREATE INDEX idx_bills_customer_id ON bills(customer_id);
 CREATE INDEX idx_bills_status ON bills(status);
 CREATE INDEX idx_bills_vendor ON bills(vendor);
-
-CREATE TABLE conversation_state (
-    conversation_id VARCHAR(128) NOT NULL,
-    state_key       VARCHAR(255) NOT NULL,
-    state_value     VARCHAR(255) NOT NULL,
-    expires_at      TIMESTAMP    NOT NULL,
-
-    PRIMARY KEY (conversation_id, state_key)
-);
-
-CREATE INDEX idx_conversation_state_expires ON conversation_state (expires_at);
