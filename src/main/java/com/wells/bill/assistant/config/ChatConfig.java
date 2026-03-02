9e19dfef547207c35d75e7becb8cd7a6ce512cfd
rdurgesh <drai548@gmail.com>
2026-03-02 21:56:34 +0530

    Integration test completed

--- src/main/java/com/wells/bill/assistant/config/ChatConfig.java
+++ src/main/java/com/wells/bill/assistant/config/ChatConfig.java
@@ -6,6 +6,7 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
@@ -35,8 +36,9 @@ public class ChatConfig {
 
         Advisor[] advisors = {
                 new SimpleLoggerAdvisor(),
-                new SafeGuardAdvisor(List.of()),
-                MessageChatMemoryAdvisor.builder(chatMemory).build()
+                new SafeGuardAdvisor(List.of("This is a safe guard message.")),
+                MessageChatMemoryAdvisor.builder(chatMemory).build(),
+                QuestionAnswerAdvisor.builder(vectorStore).build()
         };
 
         return builder.defaultAdvisors(advisors).build();
